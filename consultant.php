dbe681323a806555886f4ebb8f8c634ffe4773bf
ejacks12 <159049181+ejacks12@users.noreply.github.com>
2024-04-22 09:56:35 -0400

    Improved CSS.
    As described; changed the CSS to look nicer. I also expanded on some UI in the Consultant panel to make the page more understandable.

--- consultant.php
+++ consultant.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <head>
     <link rel="stylesheet" href="css/main.css">
-    <title>Consultation Page, Sprint 1</title>
+    <title>Consultation Page, Sprint 3</title>
     </head>
     <body>
 	    <div class="header">
@@ -13,15 +13,23 @@
 		</div>
 	
         <div class="wrapper_main">
-			<div class="wrapper_interior_a" style="width:100%; margin-bottom: 0.05%">
+			<div class="wrapper_interior_a" style="padding-left: 50px; margin-left: auto; margin-right: auto; width:50%; margin-bottom: 0.05%">
 				<h1>Consultant Panel</h1>
+				<p>In order to consult a user's reading:</p>
+				<ol>
+					<li>Look at the panel underneath this one.</li>
+					<li>A list of readings may appear automatically when you load this page: if so, each one has a button and a number.</li>
+					<li>Click the button you be directed to a page containing that user's reading information.</li>
+					<li>If nothing appears, then there are no outstanding consultations.</li>
+				</ol>
+				<a href="index.php">Go back</a>
 			</div>
 		</div>
 		
 		
 		<div class="wrapper_main">
 			<div class="wrapper_interior_a" style="width:100%; margin-top: 0.05%">
-				<p><b>Readings which require consultation:</b></p>
+				<p><b><u>Readings which require your consultation:</u></b></p>
 				<?php
 					include "consultant_handleSearch.php"
 				?>
