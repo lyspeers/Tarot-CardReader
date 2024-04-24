88be5a371e0b4252d1cf22e48ab666581be7ee8b
ejacks12 <159049181+ejacks12@users.noreply.github.com>
2024-04-24 12:04:49 -0400

    I FIXED THE TAROT DATABASE
    I also tweaked my own Consultant page with some session control stuff. It should be tweaked before it gets merged.

--- consultant.php
+++ consultant.php
@@ -1,42 +1,48 @@
 <!DOCTYPE html>
 <html>
-    <meta charset="UTF-8">
+    <meta charset='UTF-8'>
     <head>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel='stylesheet' href='css/main.css'>
     <title>Consultation Page, Sprint 3</title>
     </head>
     <body>
-	    <div class="header">
+	    <div class='header'>
 			<?php
-				include "header.php"
+				include 'header.php'
 			?>
 		</div>
-	
-        <div class="wrapper_main">
-			<div class="wrapper_interior_a" style="padding-left: 50px; margin-left: auto; margin-right: auto; width:50%; margin-bottom: 0.05%">
-				<h1>Consultant Panel</h1>
-				<p>In order to consult a user's reading:</p>
-				<ol>
-					<li>Look at the panel underneath this one.</li>
-					<li>A list of readings may appear automatically when you load this page: if so, each one has a button and a number.</li>
-					<li>Click the button you be directed to a page containing that user's reading information.</li>
-					<li>If nothing appears, then there are no outstanding consultations.</li>
-				</ol>
-				<a href="index.php">Go back</a>
-			</div>
-		</div>
 		
+		<?php 
+			
+			if ($_SESSION["power"] < 2) {
+				echo "<h1 style='font-family: tahoma; margin-left: 10%;'>404: Page not found!</h1>";
+				
+			} else {
+				        echo "<div class='wrapper_main'>";
+						echo "<div class='wrapper_interior_a' style='padding-left: 50px; margin-left: auto; margin-right: auto; width:50%; margin-bottom: 0.05%'>";
+							echo "<h1>Consultant Panel</h1>";
+							echo "<p>In order to consult a user's reading:</p>";
+							echo "<ol>";
+								echo "<li>Look at the panel underneath this one.</li>";
+								echo "<li>A list of readings may appear automatically when you load this page: if so, each one has a button and a number.</li>";
+								echo "<li>Click the button you be directed to a page containing that user's reading information.</li>";
+								echo "<li>If nothing appears, then there are no outstanding consultations.</li>";
+							echo "</ol>";
+							echo "<a href='index.php'>Go back</a>";
+						echo "</div>";
+					echo "</div>";
+					echo "<div class='wrapper_main'>";
+						echo "<div class='wrapper_interior_a' style='width:100%; margin-top: 0.05%'>";
+						echo "<p><b><u>Readings which require your consultation:</u></b></p>";
+							include 'consultant_handleSearch.php';
+								echo "</div>";
+							echo "</div>";
+						echo "</body>";
+			}
+		?>
+
+
 		
-		<div class="wrapper_main">
-			<div class="wrapper_interior_a" style="width:100%; margin-top: 0.05%">
-				<p><b><u>Readings which require your consultation:</u></b></p>
-				<?php
-					include "consultant_handleSearch.php"
-				?>
-				
-				
-				
-			</div>
-		</div>
-    </body>
+
+
 </html>
